b6e99b86f57ca432f620ac972c090bd6617d79db
glubsy <glubsy@users.noreply.github.com>
2026-06-15 20:44:19 +0200

    Container: install missing POT provider

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,8 @@ set -e
 # echo "Upgrading yt-dlp to the latest version..."
 # pip install --upgrade --no-cache-dir yt-dlp
 
+cd /opt/bgutil-ytdlp-pot-provider/server/node_modules
+deno run --allow-env --allow-net --allow-ffi=. --allow-read=. ../src/main.ts &
+
+cd /app
 exec python ./livestream_saver.py "$@"
